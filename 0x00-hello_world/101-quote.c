676ccbfd2216c80ec91db131fcd973a746927419
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-07 07:37:00 +0000

    c basics

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,8 @@
 
 int main(void)
 {
-	char *message = "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, m, 59);
 	return (1);
 }
