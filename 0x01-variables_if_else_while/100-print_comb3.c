525ca94a7c721e4c549efb5463a7c71a2bedcef0
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-08 11:38:19 +0000

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,25 @@
 
 int main(void)
 {
-	for (int i = 48; i < 58; i++)
-		for (int j = i + 1; j < 58; j++)
+	int i = 48;
+
+	while (i < 58)
+	{
+		int j = i + 1;
+
+		while (j < 58)
 		{
 			putchar(i);
 			putchar(j);
 			if (i != 56)
+			{
 				putchar(',');
-			putchar(' ');
+				putchar(' ');
+			}
+			j++;
 		}
+		i++;
+	}
 	putchar('\n');
 	return (0);
 }
