d43e34349a116e572e63bd48ce1aa0b821c90807
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-12 12:33:02 +0000

    function

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,34 @@
 
 int main(void)
 {
-	int a = 1, b = 2, x = 2;
+	unsigned long int i;
+	unsigned long int fir = 1;
+	unsigned long int sec = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%d, %d, ", a, b);
-	while (x < 98)
+	printf("%lu", fir);
+	for (i = 1; i < 91; i++)
 	{
-		int c = a + b;
-		printf("%d, ", c);
-		a = b;
-		b = c;
-		x++;
+		printf(", %lu", sec);
+		sec += fir;
+		fir = sec - fir;
+	}
+	bef1 = (fir / l);
+	bef2 = (fir % l);
+	aft1 = (sec / l);
+	aft2 = (sec % l);
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
