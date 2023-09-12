274b05602de3681b7b2328c6f12ec9f7a748801c
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-12 11:55:24 +0000

    function

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,22 @@
 
 int main(void)
 {
-	int i, n = 98, x = 1, y = 2, temp;
+	int a = 1, b = 2;
 
-	for (i = 0; i < n ; i++)
+	printf("%d, %d, ", a, b);
+
+	int x = 2;
+
+	while (x < 98)
 	{
-		printf("%d, ", x);
-		temp = x;
-
-		x = y;
-		y += temp;
+		int c = a + b;
+		
+		printf("%d, ", c);
+		
+		 a = b;
+		 b = c;
+		 x++;
 	}
 	printf("\n");
 	return (0);
-
 }
