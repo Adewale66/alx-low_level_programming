8b3d78e394b105da60489a30fb991b8aff61a0c1
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-14 08:40:39 +0000

    More functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 0.
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
