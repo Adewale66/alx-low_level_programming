d02c1e81d4a72d212676370a24eb2e32b2001ea3
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-18 07:12:08 +0000

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,6 +13,11 @@ void puts_half(char *str)
 	while (str[len] != '\0')
 		len++;
 
+	if( len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len / 2) + 1;
+
 	for (i = len / 2; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
