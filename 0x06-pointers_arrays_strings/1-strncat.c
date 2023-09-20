1d3e07579e03a08ab2166291d09cfcb75e4a4eb2
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-20 17:09:34 +0000

    pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,10 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int dest_len = 0, i;
+
 	while (dest[dest_len] != '\0')
 		dest_len++;
-	int i;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[dest_len + i] = src[i];
