45591c6bda6bdb5b5da6877b0ccfea8949e9ef4d
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-20 17:01:38 +0000

    pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string
+ * Return: char *
+ */
+
+char *cap_string(char *s)
+{
+	int len = 1;
+
+	if (s[0] > 96 && s[0] < 123)
+		s[0] -= 32;
+
+	while (s[len] != '\0')
+	{
+		int t = s[len - 1];
+
+		if (t == 32 || t == 9 || t == 10)
+		{
+			if (s[len] > 96 && s[len] < 123)
+				s[len] -= 32;
+		}
+		else if (t > 32 && t < 48)
+		{
+			if (s[len] > 96 && s[len] < 123)
+				s[len] -= 32;
+		}
+		else if ((t > 57 && t < 65) || (t > 122 && t < 126))
+		{
+			if (s[len] > 96 && s[len] < 123)
+				s[len] -= 32;
+		}
+		len++;
+	}
+
+	return (s);
+}
