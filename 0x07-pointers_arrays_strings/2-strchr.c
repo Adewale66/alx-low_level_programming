1d0db2d75e8247ced0b4e2110b158605fbb69842
Adewale Kujore <adewalekujore6@gmail.com>
2023-09-25 11:17:13 +0000

    Pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,6 +18,6 @@ char *_strchr(char *s, char c)
 			return (s + len);
 		len++;
 	}
-	return (NULL);
+	return ('\0');
 }
 
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -21,5 +21,5 @@ char *_strpbrk(char *s, char *accept)
 		len++;
 	}
 
-	return (NULL);
+	return ('\0');
 }
