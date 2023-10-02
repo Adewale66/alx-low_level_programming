9efe7eba8b9b56fd840158f2c1ed80deda6859e0
Adewale Kujore <adewalekujore6@gmail.com>
2023-10-02 08:00:52 +0000

    cli

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of args
+ * @argv: array of strings
+ * Return: 0 - success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
