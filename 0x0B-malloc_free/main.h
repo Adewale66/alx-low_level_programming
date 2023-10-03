2daa9586c2682758bb2a1f91dd24aadba1139107
Adewale Kujore <adewalekujore6@gmail.com>
2023-10-03 07:57:22 +0000

    malloc free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 int _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
-char *str_concat(char, char);
+char *str_concat(char *, char *);
 int **alloc_grid(int, int);
 void free_grid(int **, int);
 char *argstostr(int, char **);
