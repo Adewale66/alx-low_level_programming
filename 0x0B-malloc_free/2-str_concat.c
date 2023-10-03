61cf0ca47aab581e956ab06383bebd27b72cefb7
Adewale Kujore <adewalekujore6@gmail.com>
2023-10-03 08:00:22 +0000

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,19 +11,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3 = (char *) malloc(sizeof(char *));
-	int len = 0;
+	int len = 0, pos = 0;
 
 	if (s3 == NULL)
 		return (NULL);
 
-	while (s1[len] != '\0')
+	while (s1[pos] != '\0')
 	{
-		s3[len] = s1[len];
+		s3[len] = s1[pos];
 		len++;
 	}
-	while (s2[len] != '\0')
+	pos = 0
+	while (s2[pos] != '\0')
 	{
-		s3[len] = s2[len];
+		s3[len] = s2[pos];
 		len++;
 	}
 	s3[len] = '\0';
