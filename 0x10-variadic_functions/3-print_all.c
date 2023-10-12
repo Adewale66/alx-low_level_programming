117ae8983dd6f5b3383abd161834c2c77d4e47d7
Adewale Kujore <adewalekujore6@gmail.com>
2023-10-12 12:03:55 +0000

    variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,9 +34,8 @@ void print_all(const char * const format, ...)
 				case 's':
 					s = va_arg(args, char *);
 					if (s == NULL)
-						printf("(nil)%s", sep);
-					else
-						printf("%s%s", sep, s);
+						s = "(nil)";
+					printf("%s%s", sep, s);
 					break;
 				default:
 					i++;
