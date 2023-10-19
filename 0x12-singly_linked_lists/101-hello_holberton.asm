55f76c8902c2b2be2449ea7a4e97f44c4aeec98f
Adewale Kujore <adewalekujore6@gmail.com>
2023-10-19 12:50:09 +0100

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,10 @@
-global _start
+global  main
+extern printf
 
-section .text
-
-_start:
-	mov rax,1
-	mov rdi,1
-	mov rsi,greet
-	mov rdx,len
-	syscall
-
-	mov rax,60
-	mov rdi,0
-	syscall
-
-
-section .data
-	greet: db "Hello, Holberton", 0xA
-	len: equ $-greet
+main:
+	mov edi,greet
+	xor eax,eax
+	call printf
+	mov eax,0
+	ret
+greet: db `Hello, Holberton\n`,0
