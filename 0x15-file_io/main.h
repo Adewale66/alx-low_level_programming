05d2bcb06bf662a264d63893274bc9271be8e351
Adewale Kujore <adewalekujore6@gmail.com>
2023-10-30 08:04:32 +0100

    files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
+
+#endif
