326d001930dc416ed9cf7577f745bba80f795ddc
Adewale Kujore <adewalekujore6@gmail.com>
2023-12-07 14:16:43 +0100

    mandatory

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-		while (i < idx - 1)
+	while (i < idx - 1)
 	{
 		temp = temp->next;
 		if (temp == NULL)
